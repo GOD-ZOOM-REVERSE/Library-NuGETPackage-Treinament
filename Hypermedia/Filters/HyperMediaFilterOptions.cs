90bfd4da61b549f8bc6f233111aca87118b0253c
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-11-11 17:47:54 -0300

    Adicionar arquivos de projeto.

---
+++ Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary1.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace ClassLibrary1.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
